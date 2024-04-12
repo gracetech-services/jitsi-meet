2327a521aa4f9f41a639f236b07d0590faa97a03
JeffYin24 <jeffjuny@gmail.com>
2024-04-12 16:23:27 -0700

    fix lint and make errors for CI to pass

--- react/features/base/ui/components/updateTheme.native.ts
+++ react/features/base/ui/components/updateTheme.native.ts
@@ -5,8 +5,8 @@
  * @returns {Object}
  */
 export default function updateTheme(theme: Object) {
-    //Gracetech -- to compact more buttons 
-    theme.spacing[2] = 2;
-    theme.spacing[3] = 2;
+    // Gracetech -- to compact more buttons
+    // theme.spacing[2] = 2;
+    // theme.spacing[3] = 2;
     return theme;
 }
