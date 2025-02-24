02e841b694d4c0f6e7e1ba5ef243b7152529d20b
Ranger8822 <wangjincrazy007@gmail.com>
2025-02-25 00:27:31 +0800

    feat: add style files for FishMeet

---
+++ react/features/mobile/navigation/components/conference/components/fishMeetNavigationStyles.ts
@@ -0,0 +1,23 @@
+import BaseTheme from '../../../../../base/ui/components/BaseTheme.native';
+
+export const styleHeader = {
+    viewStyle: {
+        backgroundColor: BaseTheme.palette.fishMeetUiBackground,
+        height: 60,
+        borderTopLeftRadius: 30,
+        borderTopRightRadius: 30,
+        justifyContent: 'center',
+        alignItems: 'center',
+        flexDirection: 'row'
+    },
+    textStyle: {
+        color: BaseTheme.palette.text01,
+        fontSize: 18,
+        flex: 1,
+        textAlign: 'center'
+    },
+    touchStyle: {
+        position: 'absolute',
+        right: 30
+    }
+};
---
+++ react/features/mobile/navigation/fishMeetScreenOptions.ts
@@ -0,0 +1,20 @@
+import BaseTheme from '../../base/ui/components/BaseTheme.native';
+
+export const fishMeetNavigationContainerTheme = {
+    colors: {
+        background: 'transparent'
+    }
+};
+
+
+export const fishMeetChatTabBarOptions = {
+    swipeEnabled: false,
+    tabBarIndicatorStyle: {
+        backgroundColor: BaseTheme.palette.fishMeetMainColor02
+    },
+    tabBarStyle: {
+        backgroundColor: BaseTheme.palette.fishMeetUiBackground,
+        borderBottomColor: '#C8D7EC80',
+        borderBottomWidth: 0.4
+    }
+};
