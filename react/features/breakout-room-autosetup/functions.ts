24d05690854de63a7bbe278c0dc779b14be30ef7
Max Peng <penjuntan@gmail.com>
2025-08-14 14:01:33 +0800

    feat(breakout-rooms): update UI text and improve room setup logic
    - Update UI text for auto and preset breakout room buttons
    - Add participant count check for auto and preset room setup
    - Refactor getAllParticipants function to improve code reusability

--- react/features/breakout-room-autosetup/functions.ts
+++ react/features/breakout-room-autosetup/functions.ts
@@ -3,6 +3,7 @@ import { size } from 'lodash-es';
 import { IStateful } from '../base/app/types';
 import { isLocalParticipantModerator } from '../base/participants/functions';
 import { toState } from '../base/redux/functions';
+import { getAllParticipants } from '../breakout-room-presetup/functions';
 import { getBreakoutRooms, getBreakoutRoomsConfig } from '../breakout-rooms/functions';
 
 export const isAutoBreakoutRoomButtonVisible = (stateful: IStateful) => {
@@ -11,8 +12,9 @@ export const isAutoBreakoutRoomButtonVisible = (stateful: IStateful) => {
     const { conference } = state['features/base/conference'];
     const isBreakoutRoomsSupported = conference?.getBreakoutRooms()?.isSupported();
     const { hideAutoAssignButton } = getBreakoutRoomsConfig(state);
+    const allParticipantsSize = size(getAllParticipants(stateful));
 
-    return isLocalModerator && isBreakoutRoomsSupported && !hideAutoAssignButton;
+    return isLocalModerator && isBreakoutRoomsSupported && !hideAutoAssignButton && allParticipantsSize > 3;
 };
 
 export const isCloseAllBreakoutRoomVisible = (stateful: IStateful) => {
